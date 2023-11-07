3769fb3d4cef4de392a01d7859a671144f66da3a
MitsukiS16 <clarissemtcarvalho@gmail.com>
2023-11-07 20:22:43 +0000

    add database

---
+++ db/schema.sql
@@ -0,0 +1,115 @@
+CREATE TABLE IF NOT EXISTS SCHOOL (
+    id_school INTEGER NOT NULL PRIMARY KEY,
+    name_school VARCHAR(50) NOT NULL,
+    address_school VARCHAR(50),
+    website VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS USERS (
+    id_user INTEGER NOT NULL PRIMARY KEY,
+    id_school INTEGER,
+    id_course INTEGER NOT NULL,
+    id_enrolled INTEGER NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    pw_hash BINARY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    picture BLOB,
+    role_type TEXT CHECK(role_type IN ('student', 'teacher', 'admin')) NOT NULL DEFAULT 'student',
+    created_at DATETIME DEFAULT (datetime('now')),
+    FOREIGN KEY(id_school) REFERENCES SCHOOL(id_school),
+    FOREIGN KEY(id_course) REFERENCES COURSE(id_course),
+    FOREIGN KEY(id_enrolled) REFERENCES ENROLLED(id_enrolled)
+);
+
+CREATE TABLE IF NOT EXISTS ENROLLED (
+    id_enrolled INTEGER NOT NULL PRIMARY KEY,
+    year_enrolled YEAR NOT NULL,
+    trimester_enrolled INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS COURSE (
+    id_course INTEGER NOT NULL PRIMARY KEY,
+    name_course VARCHAR(30),
+    number_subject INT
+);
+
+CREATE TABLE IF NOT EXISTS SUBJECTSTUDENT (
+    id_subject INTEGER NOT NULL PRIMARY KEY,
+    id_teacher INTEGER NOT NULL,
+    name_subject VARCHAR(30) NOT NULL,
+    student_count INT,
+    happiness INT,
+    subject_status BOOLEAN NOT NULL DEFAULT 'TRUE',
+    FOREIGN KEY(id_teacher) REFERENCES TEACHER(id_teacher)
+);
+
+CREATE TABLE IF NOT EXISTS COURSESUBJECT (
+    id_course INTEGER NOT NULL,
+    id_subject INTEGER NOT NULL,
+    FOREIGN KEY(id_course) REFERENCES COURSE(id_course),
+    FOREIGN KEY(id_subject) REFERENCES SUBJECTSTUDENT(id_subject),
+    CONSTRAINT course_subject_pk PRIMARY KEY (id_course, id_subject)
+);
+
+CREATE TABLE IF NOT EXISTS EVALUATION (
+    id_evaluation INTEGER NOT NULL PRIMARY KEY,
+    name_evaluation VARCHAR(30) NOT NULL,
+    type_evaluation TEXT CHECK(type_evaluation IN ('teste', 'qa', 'cidadania', 'apresentacao', 'relatorio', 'fichas', 'tpc', 'composição', 'trabalho', 'outro')) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS SUBJECTEVALUATION (
+    id_subject INTEGER NOT NULL,
+    id_evaluation INTEGER NOT NULL,
+    FOREIGN KEY(id_subject) REFERENCES SUBJECTSTUDENT(id_subject),
+    FOREIGN KEY(id_evaluation) REFERENCES EVALUATION(id_evaluation),
+    CONSTRAINT subject_evaluation_pk PRIMARY KEY (id_subject, id_evaluation)
+);
+
+CREATE TABLE IF NOT EXISTS GRADE (
+    id_grade INTEGER NOT NULL PRIMARY KEY,
+    score FLOAT,
+    expected_score FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS EVALUATIONGRADE (
+    id_evaluation INTEGER NOT NULL,
+    id_grade INTEGER NOT NULL,
+    date_evaluation DATETIME,
+    FOREIGN KEY(id_evaluation) REFERENCES EVALUATION(id_evaluation),
+    FOREIGN KEY(id_grade) REFERENCES GRADE(id_grade),
+    CONSTRAINT evalution_grade_pk PRIMARY KEY (id_evaluation, id_grade)
+);
+
+
+CREATE TABLE IF NOT EXISTS TEACHER (
+    id_teacher INTEGER NOT NULL PRIMARY KEY,
+    name_teacher VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS STUDENTREPORT (
+    id_student_report INTEGER NOT NULL PRIMARY KEY,
+    average_grade INT,
+    class_rank INT,
+    description_student_report TEXT,
+    created_at DATETIME DEFAULT (datetime('now')),
+    updated_at DATETIME DEFAULT (datetime('now'))
+);
+
+CREATE TABLE IF NOT EXISTS REPORTCARD (
+    id_report_card INTEGER NOT NULL PRIMARY KEY,
+    id_subject INTEGER NOT NULL,
+    created_at DATETIME DEFAULT (datetime('now')),
+    updated_at DATETIME DEFAULT (datetime('now')),
+    description_report_card TEXT,
+    FOREIGN KEY(id_subject) REFERENCES SUBJECTSTUDENT(id_subject)
+);
+
+CREATE TABLE IF NOT EXISTS STUDENTREPORTCARD (
+    id_student_report INTEGER NOT NULL,
+    id_report_card INTEGER NOT NULL,
+    FOREIGN KEY(id_student_report) REFERENCES STUDENTREPORT(id_student_report),
+    FOREIGN KEY(id_report_card) REFERENCES REPORTCARD(id_report_card),
+    CONSTRAINT student_report_card_pk PRIMARY KEY (id_student_report, id_report_card)
+);
---
+++ db/seed.sql
@@ -0,0 +1,123 @@
+DELETE FROM SCHOOL
+DELETE FROM USERS
+DELETE FROM ENROLLED
+DELETE FROM COURSE
+DELETE FROM SUBJECTSTUDENT
+DELETE FROM COURSESUBJECT
+DELETE FROM EVALUATION
+DELETE FROM SUBJECTEVALUATION
+DELETE FROM GRADE 
+DELETE FROM EVALUATIONGRADE
+DELETE FROM TEACHER
+DELETE FROM STUDENTREPORT
+DELETE FROM REPORTCARD
+DELETE FROM STUDENTREPORTCARD
+
+
+VACUUM;
+
+INSERT INTO SCHOOL VALUES ( 
+    1,
+    'school example',
+    'address example',
+    'websiteschool.com'
+);
+
+
+INSERT INTO USERS VALUES ( 
+    1,
+    1,
+    1,
+    1,
+    'admin',
+    'admin@gmail.com',
+    '$2y$10$CmA8X7Dxwq6FFJbyLfdjNuVrQLtCyrmHkdeVZcf9pssozaCM3rdyK',
+    'admin'
+    'admin'
+    'https://icons-for-free.com/iconfiles/png/512/man+person+profile+user+worker+icon-1320190557331309792.png',
+    'admin',
+    '2023-01-01'
+);
+
+INSERT INTO ENROLLED VALUES ( 
+    1,
+    1,
+    1
+);
+
+INSERT INTO COURSE VALUES ( 
+    1,
+    'primaria',
+    1
+);
+
+INSERT INTO SUBJECTSTUDENT VALUES ( 
+    1,
+    1,
+    'matematica',
+    30,
+    4,
+    TRUE
+);
+
+INSERT INTO COURSESUBJECT VALUES (
+    1,
+    1
+);
+
+INSERT INTO EVALUATION VALUES ( 
+    1,
+    'testes 1 periodo',
+    type_evaluation VARCHAR(30) NOT NULL,
+);
+
+INSERT INTO SUBJECTEVALUATION VALUES (
+    1,
+    1
+);
+
+INSERT INTO GRADE VALUES (
+    1,
+    1.1,
+    2.5
+
+);
+
+INSERT INTO EVALUATIONGRADE VALUES (
+    1,
+    1
+    '2023-01-01 00:00:00'
+
+);
+
+INSERT INTO TEACHER VALUES ( 
+    1,
+    'sara'
+);
+
+
+INSERT INTO STUDENTREPORT VALUES ( 
+    1,
+    1,
+    3,
+    10,
+    'admin organizado',
+    '2023-01-01 00:00:00',
+    '2023-01-01 00:00:01'
+);
+
+INSERT INTO REPORTCARD VALUES ( 
+    1,
+    1,
+    '2023-01-01 00:00:00',
+    '2023-01-01 00:00:01'
+    'report card admin',
+);
+
+
+INSERT INTO STUDENTREPORTCARDS VALUES (
+    1,
+    1
+); 
+
+
